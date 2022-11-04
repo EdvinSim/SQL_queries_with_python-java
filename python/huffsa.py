e93eb229c6403e1f3d25cda65eadd0ba6b3fd6a4
EdvinSim <edvin0106@hotmail.com>
2022-11-04 14:58:51 +0100

    Oppgave 1

--- python/huffsa.py
+++ python/huffsa.py
@@ -1,7 +1,6 @@
 import psycopg2
 
 # MERK: Må kjøres med Python 3
-#Oppdatert TODO fjern
 
 user = 'edvinas' # Sett inn ditt UiO-brukernavn ("_priv" blir lagt til under)
 pwd = 'avahreeW2e' # Sett inn passordet for _priv-brukeren du fikk i en mail
@@ -15,21 +14,43 @@ connection = \
 
 def huffsa():
     conn = psycopg2.connect(connection)
-    
-    ch = 0
-    while (ch != 3):
-        print("--[ HUFFSA ]--")
-        print("Vennligst velg et alternativ:\n 1. Søk etter planet\n 2. Legg inn forsøksresultat\n 3. Avslutt")
-        ch = int(input("Valg: "))
 
-        if (ch == 1):
+    ch = 0
+    while ch != "3":
+        print("\n--[ HUFFSA ]--")
+        print("Vennligst velg et alternativ:\n 1. Søk etter planet\n 2. Legg inn forsøksresultat\n 3. Avslutt")
+
+        ch = input("Valg: ")
+
+        if ch == "1":
             planet_sok(conn)
-        elif (ch == 2):
+        elif ch == "2":
             legg_inn_resultat(conn)
-    
-def planet_sok(conn):
-    # TODO: Oppg 1
-    return
+        elif ch == "3":
+            print("Hadet bra :)")
+        else:
+            print("\nUgyldig kommando!")
+
+def planet_sok(conn: psycopg2):
+    cur = conn.cursor()
+    molekyl1 = input("Molekyl:")
+    molekyl2 = input("Skriv et molekyl til eller trykk enter: ")
+    sporring = f"SELECT DISTINCT p.navn, p.masse, s.masse , s.avstand, liv\
+            FROM materie AS m\
+                INNER JOIN planet AS p ON (m.planet = p.navn)\
+                INNER JOIN stjerne AS s ON (p.stjerne = s.navn)\
+            WHERE molekyl LIKE '%{molekyl1}%' AND molekyl LIKE '%{molekyl2}%'\
+            ORDER BY p.navn;"
+
+    cur.execute(sporring)
+
+    for rad in cur.fetchall():
+        if rad[4] == True:
+            liv = "Ja"
+        else:
+            liv = "Nei"
+        print(f"\n--PLANET--\nNavn: {rad[0]}\nPlanet-masse: {rad[1]}\
+        \nStjerne-masse: {rad[2]}\nStjerne-distanse: {rad[3]}\nBekreftet liv: {liv}")
 
 def legg_inn_resultat(conn):
     # TODO: Oppg 2
